d71aaef7900aea4c5aa9fa5cbdf00cdc90c33130
Uchechi090 <uchechiegbuta@gmail.com>
2023-06-10 13:40:01 +0200

    update of query parameters

--- src/store/music/actions.js
+++ src/store/music/actions.js
@@ -2,10 +2,11 @@ import axios from 'axios'
 
 export const actions = {
   async doSearch(ctx, searchTerm) {
+    const url = `https://itunes.apple.com/search?term=${searchTerm}&media=music&entity=musicArtist&entity=album&entity=song&limit=20`
 
     try {
       await axios
-        .get(`https://itunes.apple.com/search?term=${searchTerm}&media=music&limit=20`)
+        .get(url)
         .then((res) => {
             console.log(res)
             if (res.data.results.length > 0) {
